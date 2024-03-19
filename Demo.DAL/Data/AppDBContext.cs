1cbe4eb77453712fac284c68635c5c45ea4f361e
HP <HP@DESKTOP-GR1U1GH>
2024-03-19 21:07:18 +0200

    DAL_Module

---
+++ Demo.DAL/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Data
+{
+    internal class AppDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = . ; Database = MVCApp ; Trusted_Connection = true");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
