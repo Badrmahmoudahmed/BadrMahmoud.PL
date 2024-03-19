cbf7ba5e1ef42dfd0cd8392a4deb07d22189f7b4
HP <HP@DESKTOP-GR1U1GH>
2024-03-19 23:50:49 +0200

    DependancyInjection

--- BadrMahmoud.PL/Startup.cs
+++ BadrMahmoud.PL/Startup.cs
@@ -1,6 +1,8 @@
+using Demo.DAL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,7 @@ namespace BadrMahmoud.PL
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AppDBContext>(options => options.UseSqlServer("Server = . ; Database = MVCApp ; Trusted_Connection = true"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Demo.DAL/Data/AppDBContext.cs
+++ Demo.DAL/Data/AppDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Demo.DAL.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +9,17 @@ using System.Threading.Tasks;
 
 namespace Demo.DAL.Data
 {
-    internal class AppDBContext : DbContext
+    public class AppDBContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseSqlServer("Server = . ; Database = MVCApp ; Trusted_Connection = true");
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+            
+        }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        public DbSet<Department> Departments { get; set; }
     }
 }
