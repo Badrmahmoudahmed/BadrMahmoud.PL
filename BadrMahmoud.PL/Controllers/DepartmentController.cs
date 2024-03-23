bc2fb6ed98523fe21707c2459426ac6554419853
HP <HP@DESKTOP-GR1U1GH>
2024-03-23 15:31:23 +0200

    EditAction

--- BadrMahmoud.PL/Controllers/DepartmentController.cs
+++ BadrMahmoud.PL/Controllers/DepartmentController.cs
@@ -1,17 +1,22 @@
 ﻿using Demo.BLL.Interfaces;
 using Demo.BLL.Repositries;
 using Demo.DAL.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace BadrMahmoud.PL.Controllers
 {
     public class DepartmentController : Controller
     {
         private readonly IDepartmentRepositries _departmentRepositries;
+        private readonly IWebHostEnvironment _env;
 
-        public DepartmentController(IDepartmentRepositries departmentRepositries)
+        public DepartmentController(IDepartmentRepositries departmentRepositries , IWebHostEnvironment env)
         {
             _departmentRepositries = departmentRepositries;
+            _env = env;
         }
         public IActionResult Index()
         {
@@ -37,7 +42,7 @@ namespace BadrMahmoud.PL.Controllers
 
         [HttpGet]
 
-        public IActionResult Details(int? id)
+        public IActionResult Details(int? id , string viewname = "Details")
         {
             if (!id.HasValue)
             {
@@ -50,7 +55,45 @@ namespace BadrMahmoud.PL.Controllers
                 return NotFound();
             }
 
-            return View(department);
+            return View(viewname,department);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id) 
+        {
+            return Details(id, "Edit");
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute]int id,Department department)
+        {
+            if (id != department.Id)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid) 
+            {
+                return View(department);
+            }
+
+            try
+            {
+                _departmentRepositries.Update(department);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                if(_env.IsDevelopment())
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "There is an Error");
+                }
+                return View(department);
+            }
         }
     }
 }
