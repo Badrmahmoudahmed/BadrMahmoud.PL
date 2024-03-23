a2e72444a37fd01436b9fa88d0880b66cd91a949
HP <HP@DESKTOP-GR1U1GH>
2024-03-23 16:02:12 +0200

    ValidateAntiForgeryToken

--- BadrMahmoud.PL/Controllers/DepartmentController.cs
+++ BadrMahmoud.PL/Controllers/DepartmentController.cs
@@ -66,6 +66,7 @@ namespace BadrMahmoud.PL.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Edit([FromRoute]int id,Department department)
         {
             if (id != department.Id)
