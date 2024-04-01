ea8296c2e5b9a8018cab656b901984a847e5f17a
HP <HP@DESKTOP-GR1U1GH>
2024-04-01 20:52:25 +0200

    DocumentSetting

---
+++ BadrMahmoud.PL/Helpers/DocumentSetting.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace BadrMahmoud.PL.Helpers
+{
+    public static class DocumentSetting
+    {
+        public static string UploadFile(IFormFile file , string foldername)
+        {
+            string folderpath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot\\Files", foldername);
+
+            if (!Directory.Exists(folderpath)) 
+            {
+                Directory.CreateDirectory(folderpath);
+            }
+
+            string filename = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+            string filepath = Path.Combine(folderpath,filename);
+
+            using var filestream = new FileStream(filepath, FileMode.Create);
+
+            file.CopyTo(filestream);
+
+            return filename;
+        }
+
+        public static void DeleteFile(string filename , string foldername) 
+        {
+            if(filename is not null && foldername is not null)
+            {
+                string filepath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", foldername, filename);
+                if (Directory.Exists(filepath))
+                {
+                    Directory.Delete(filepath);
+                }
+            }
+        }
+    }
+}
