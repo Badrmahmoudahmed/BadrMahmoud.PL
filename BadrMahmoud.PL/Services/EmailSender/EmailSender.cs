e49f006439e737059fc25313f68b1b366bcb154c
HP <HP@DESKTOP-GR1U1GH>
2024-04-25 20:18:26 +0200

    EmailSender

---
+++ BadrMahmoud.PL/Services/EmailSender/EmailSender.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace BadrMahmoud.PL.Services.EmailSender
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendAsync(string from, string reciptients, string body)
+        {
+            var SenderEmail = _configuration["EmailSetting : EmailSender"];
+            var SenderPassword = _configuration["EmailSetting : EmailPassword"];
+            var emailmsg = new MailMessage();
+            emailmsg.From = new MailAddress(from);
+            emailmsg.To.Add(reciptients);
+            emailmsg.Body = body;
+            emailmsg.IsBodyHtml = false;
+
+            var smtpclient = new SmtpClient(_configuration["Emailhost : EmailPassword"],  int.Parse(_configuration["Emailhost : Emailport"]));
+            await smtpclient.SendMailAsync(emailmsg);
+
+        }
+    }
+}
---
+++ BadrMahmoud.PL/Services/EmailSender/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace BadrMahmoud.PL.Services.EmailSender
+{
+    public interface IEmailSender
+    {
+        Task SendAsync(string from, string reciptients, string body);
+    }
+}
